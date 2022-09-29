3df553eac2ba19a2ed0fd963ec9f718ee6e3c78f
Thalhath A <thalhath.az@pitsolutions.com>
2022-09-29 16:53:14 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,54 @@
+function ac(){
+    document.getElementById('screen').innerHTML=' '
+}
+function mod(){
+    document.getElementById('screen').innerHTML+='%'
+}
+function division(){
+    document.getElementById('screen').innerHTML+="/"
+}
+function onee(){
+    document.getElementById('screen').innerHTML+=1
+}
+function to(){
+    document.getElementById('screen').innerHTML+=2
+}
+function three(){
+    document.getElementById('screen').innerHTML+=3
+}
+function multiple(){
+    document.getElementById('screen').innerHTML+='*'
+}
+function fore(){
+    document.getElementById('screen').innerHTML+=4
+}
+function fiv(){
+    document.getElementById('screen').innerHTML+=5
+}
+function sics(){
+    document.getElementById('screen').innerHTML+=6
+}
+function minus(){
+    document.getElementById('screen').innerHTML+='-'
+}
+function sevn(){
+    document.getElementById('screen').innerHTML+=7
+}
+function eit(){
+    document.getElementById('screen').innerHTML+=8
+}
+function nin(){
+    document.getElementById('screen').innerHTML+=9
+}
+function plus(){
+    document.getElementById('screen').innerHTML+='+'
+}
+function zeroo(){
+    document.getElementById('screen').innerHTML+=0
+}
+function point(){
+    document.getElementById('screen').innerHTML+='.'
+}
+function equal(){
+    document.getElementById('screen').innerHTML=eval(document.getElementById('screen').innerHTML)
+}
